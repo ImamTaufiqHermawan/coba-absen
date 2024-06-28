d7094ff0b077a4984036810209600e74fae9c96d
ImamTaufiqHermawan <imamtaufiq133@gmail.com>
2024-06-28 10:15:09 +0700

    add API get chat id

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const fetch = require("node-fetch");
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 const CHAT_ID = process.env.CHAT_ID;
+
+const app = express();
+app.use(bodyParser.json());
 
 const sendPoll = async () => {
   const url = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendPoll`;
@@ -16,24 +19,27 @@ const sendPoll = async () => {
   };
 
   try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(pollData),
-    });
-
-    if (!response.ok) {
-      throw new Error(`Error: ${response.statusText}`);
+    const response = await axios.post(url, pollData);
+    if (response.data.ok) {
+      console.log("Poll sent successfully");
+    } else {
+      console.error("Failed to send poll", response.data);
     }
-
-    console.log("Poll sent successfully");
   } catch (error) {
     console.error("Failed to send poll", error);
   }
 };
 
-const app = express();
-app.use(bodyParser.json());
+app.post(`/bot${TELEGRAM_TOKEN}`, (req, res) => {
+  const message = req.body.message;
+  if (message) {
+    const chatId = message.chat.id;
+    console.log("Chat ID:", chatId);
+    res.send("Chat ID logged in console.");
+  } else {
+    res.send("No message received.");
+  }
+});
 
 app.get("/send-poll", async (req, res) => {
   await sendPoll();
@@ -44,3 +50,15 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const setWebhook = async () => {
+  const url = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/setWebhook?url=https://coba-absen.onrender.com/bot${TELEGRAM_TOKEN}`;
+  try {
+    const response = await axios.get(url);
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error setting webhook:", error);
+  }
+};
+
+setWebhook();
